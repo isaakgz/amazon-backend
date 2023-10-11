e367089b449cfa2824536f06d35cbb64a97730d5
isaak gz <yesakgz12@gmail.com>
2023-10-11 09:23:34 +0300

    update

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ require("dotenv").config();
 // const logger = require("firebase-functions/logger");
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")(process.env.SECRETE_KEY);
+const stripe = require("stripe")(
+  "sk_test_51NwjmXDLellSq864XgxT0hqqYB4VuxyM2z0JZuAJLSqSY8PEkMulXQ3che5CF2sgZm0BepkAnnLsrFZ3hy5DzDft00Xu4StqJj"
+);
 
 const app = express();
 app.use(cors());
@@ -12,13 +14,11 @@ app.use(cors({ origin: true }));
 app.use(express.json());
 
 app.get("/", (request, response) => response.status(200).send("Hello World"));
-app.get("/isaak", (request, response) =>
-  response.status(200).send("Evangadi")
-);
+app.get("/isaak", (request, response) => response.status(200).send("Evangadi"));
 
 app.post("/payments/create", async (request, response) => {
   const total = request.query.total;
-  
+
   if (total > 0) {
     const payementIntent = await stripe.paymentIntents.create({
       amount: total,
@@ -35,18 +35,15 @@ app.post("/payments/create", async (request, response) => {
     });
   }
 
+  // const total = request.query.total;
 
+  // console.log("Payment Request Recieved for this amount >>>", total);
 
-
-    // const total = request.query.total;
-  
-    // console.log("Payment Request Recieved for this amount >>>", total);
-  
-    // const paymentIntent = await stripe.paymentIntents.create({
-    //   amount: total,
-    //   currency: "usd",
-    // });
-    // // OK - Created
-    // response.status(201).send({ clientSecret: paymentIntent.client_secret });
-  });
-  exports.api = functions.https.onRequest(app);
+  // const paymentIntent = await stripe.paymentIntents.create({
+  //   amount: total,
+  //   currency: "usd",
+  // });
+  // // OK - Created
+  // response.status(201).send({ clientSecret: paymentIntent.client_secret });
+});
+exports.api = functions.https.onRequest(app);
